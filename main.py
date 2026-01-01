ef95c93d0fdd0f84830e4b99d81b365a0272bf62
brightyorcerf <thetejaansh@gmail.com>
2026-01-01 14:27:50 +0530

    final

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
-from stats import count, countChar
+import sys
+from stats import count, countChar, getSortedList
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-
 def main():
-    textPath = "books/frankenstein.txt"
-    text = get_book_text(textPath) 
-    num_words = count(text)
-    print(f"Found {num_words} total words")
+    # 1. check if the user provided a file path
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
-    dictionary = countChar(text)
-    print(dictionary)
- 
-main()
+    # 2. grab the path from the arguments list
+    book_path = sys.argv[1]
+    
+    # 3. use that path to get the text
+    text = get_book_text(book_path) 
+
+    # 4. process the data
+    word_count = count(text)
+    chars_dict = countChar(text)
+    sorted_chars = getSortedList(chars_dict) 
+
+    # 5. print report
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+
+    for item in sorted_chars:
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
+if __name__ == "__main__":
+    main()
--- stats.py
+++ stats.py
@@ -5,10 +5,23 @@ def count(text):
 def countChar(text):
     counts = {}
     text = text.lower()
-
     for char in text:
         if char in counts:
             counts[char] += 1
         else:
             counts[char] = 1
     return counts
+
+def sort_on(dict_item):
+    return dict_item["num"]
+
+def getSortedList(char_dict):
+    sorted_list = [] 
+    
+    for char, count in char_dict.items():
+        if char.isalpha():
+            new_dict = {"char": char, "num": count}
+            sorted_list.append(new_dict)
+    
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
